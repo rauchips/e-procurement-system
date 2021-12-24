fa6d0ba8e86ff0257bbcba3a434b9b9e07f9287a
rauchips <chipindebenjamin@gmail.com>
2021-12-24 10:49:31 +0300

    admin setup fixes

--- backend/controllers/admin/adminController.js
+++ backend/controllers/admin/adminController.js
@@ -107,9 +107,13 @@ exports.patchEntity = async (req, res, next) => {
   try {
     
     await Gov.updateOne({ _id: req.params.id },{ $set: {
+      'entity': req.body.entity,
+      'telephone': req.body.telephone,
+      'county': req.body.county,
+      'address': req.body.address,
+      'website': req.body.website,
       'representative.name': req.body.representative.name,
       'representative.email': req.body.representative.email,
-      'entity': req.body.entity
     } })
       .then(result => {
         if(!result) return res.json({ message: 'Entity does not exist' })
@@ -184,9 +188,11 @@ exports.patchVendor = async (req, res, next) => {
   try {
     
     await Vendor.updateOne({ _id: req.params.id },{ $set: {
+      'company': req.body.company,
+      'telephone': req.body.telephone,
+      'address': req.body.address,
       'representative.name': req.body.representative.name,
       'representative.email': req.body.representative.email,
-      'company': req.body.company
     } })
       .then(result => {
         if(!result) return res.json({ message: 'Vendor does not exist' })
@@ -260,6 +266,41 @@ exports.getTender = async (req, res, next) => {
   }
 }
 
+exports.patchTender = async (req, res, next) => {
+  try {
+
+    await Tender.updateOne({ _id: req.params.id },{ $set: {
+      'title': req.body.title,
+      'filename': req.file.filename,
+      'closingAt': req.body.closingAt,
+    } })
+      .then(result => {
+        if(!result) return res.json({ message: 'Tender does not exist' })
+        return res.status(200).json(result)
+      })
+      .catch(err => res.json(err))
+    
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+exports.deleteTender = async (req, res, next) => {
+  try {
+
+    await Tender.deleteOne({ _id: req.params.id })
+    .then((result) => {
+      if(!result) return res.json({ message: 'Tender does not exist' })
+      return res.status(200).json({ message: 'Tender has been deleted' })
+    })
+    
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
 /*
   COMMITTEE SETUP
 */
@@ -289,7 +330,7 @@ exports.getCommittee = async (req, res, next) => {
 
     await Committee.findOne({ _id: req.params.id })
       .then((result) => {
-        if(!result) return res.json({ message: 'Entity does not exist' })
+        if(!result) return res.json({ message: 'Committee does not exist' })
         res.status(200).json(result)
       })
       .catch(error => console.error(error))
@@ -305,6 +346,7 @@ exports.patchCommittee = async (req, res, next) => {
     
     await Committee.updateOne({ _id: req.params.id },{ $set: {
       'name': req.body.name,
+      'telephone': req.body.telephone,
       'email': req.body.email
     } })
       .then(result => {
@@ -334,7 +376,10 @@ exports.deleteCommittee = async (req, res, next) => {
   }
 }
 
-//get all bids
+/*
+  BID SETUP
+*/
+
 exports.getAllBids = async (req, res, next) => {
   try {
     
@@ -345,6 +390,59 @@ exports.getAllBids = async (req, res, next) => {
         body: result
       }))
       .catch(error => console.error(error))
+
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+/*
+  BID BY ID SETUP
+*/
+
+exports.getBid = async (req, res, next) => {
+  try {
+
+    await Bid.findOne({ _id: req.params.id })
+      .then((result) => {
+        if(!result) return res.json({ message: 'Bid does not exist' })
+        res.status(200).json(result)
+      })
+      .catch(error => console.error(error))
+    
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+exports.patchBid = async (req, res, next) => {
+  try {
+    
+    await Bid.updateOne({ _id: req.params.id },{ $set: {
+      'filename': req.file.filename,
+    } })
+      .then(result => {
+        if(!result) return res.json({ message: 'Bid does not exist' })
+        return res.status(200).json(result)
+      })
+      .catch(err => res.json(err))
+
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+exports.deleteBid = async (req, res, next) => {
+  try {
+    
+    await Bid.deleteOne({ _id: req.params.id })
+      .then((result) => {
+        if(!result) return res.json({ message: 'Bid does not exist' })
+        return res.status(200).json({ message: 'Bid has been deleted' })
+      })
 
   } catch (error) {
     console.error(error);
